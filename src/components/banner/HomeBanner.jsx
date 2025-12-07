12ccc2a19384b115df7a03ea7f40b249fb90355f
Alireza <aliareya2003@gmail.com>
2025-12-07 17:55:19 +0430

    ff

--- src/components/banner/HomeBanner.jsx
+++ src/components/banner/HomeBanner.jsx
@@ -13,22 +13,19 @@ function HomeBanner() {
       className="w-full xl:h-[670px] lg:h-[600px]  flex flex-col justify-center items-center bg-top bg-cover"
       style={{ backgroundImage: `url(${bg})` }}
     >
-      <div className="w-full h-[670px] gradient-light flex pt-10 lg:pt-16 xl:pt-32">
+      <div className="w-full h-[670px] gradient-light flex pt-10 lg:pt-24 xl:pt-32">
         <div className="w-1/2  flex flex-col pl-20 lg:pl-12">
           <h1 className="xl:text-[70px] lg:text-[50px] text-3xl  text-white font-semibold font-[sans-serif] text-gradient">
             Travel in Comfort,
           </h1>
-          <div className="flex mt-5 mb-10">
-            <span className="xl:text-[70px] lg:text-[40px] text-3xl  text-[#4bbebe] font-semibold font-[sans-serif] pr-3">
+          <div className="flex mt-10 lg:mt-5 mb-10">
+            <span className="xl:text-[50px] lg:text-[40px] text-3xl  text-[#4bbebe] font-semibold font-[sans-serif] pr-3">
               Arrive with
             </span>
-            <SplitText
-              text="  Star Line"
-              className="xl:text-[50px] lg:text-[50px] text-3xl text-[#ffffff] font-semibold "
-              rootMargin=""
-              textAlign="center"
-              onLetterAnimationComplete={handleAnimationComplete}
-            />
+            <span className="xl:text-[50px] lg:text-[40px] text-3xl  text-[#ffffff] font-semibold font-[sans-serif] pr-3">
+              Star Line
+            </span>
+            
           </div>
           <p className="xl:text-lg lg:text-sm text-sm text-white/90 font-medium font-[sans-serif]">
             Affordable prices meet premium comfort. Our easy-to-use system helps<br></br>
